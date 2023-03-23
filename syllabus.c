5396166fa9748f5219fabf450a1c54640f33c57f
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-03-23 16:47:21 +0530

    Update and rename 1.c to syllabus.c

---
+++ syllabus.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main ()
+{
+  printf("CA3 on 23.03.2023");
+  printf("Module 1. Introduction to Programming\nModule 2. Arithmetic Expressions and Precedence\nModule 3. Conditional Branching and Loops");
