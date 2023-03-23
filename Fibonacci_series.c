fcb07fb356e859d228a7a4f0e3cc8d7a7e39dbd6
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2023-03-23 22:31:45 +0530

    Update Fibonacci_series.c

--- Fibonacci_series.c
+++ Fibonacci_series.c
@@ -2,3 +2,17 @@
 
 int main ()
 {
+  int a, b, i, n, result;
+  printf("provide number : ");
+  scanf("%d", &n);
+  a=0;
+  b=1;
+  for (i=1; i<=n; i++)
+  {
+    printf("%d", a);
+    result = a+b;
+    a=b;
+    b=result;
+  }
+  return 0;
+}
